81ed2e4a8ae966472722581568f2d418ec01fb29
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-10-14 15:34:02 +0900

    Add getCompletePosition

--- denops/@ddc-sources/git-file.ts
+++ denops/@ddc-sources/git-file.ts
@@ -7,14 +7,42 @@ import {
   BaseSource,
   Candidate,
 } from "https://lib.deno.dev/x/ddc_vim@v0/types.ts";
-import { GatherCandidatesArguments } from "https://lib.deno.dev/x/ddc_vim@v0/base/source.ts";
+import {
+  GatherCandidatesArguments,
+  GetCompletePositionArguments,
+} from "https://lib.deno.dev/x/ddc_vim@v0/base/source.ts";
 import * as fn from "https://deno.land/x/denops_std@v2.1.1/function/mod.ts";
 import { git } from "../../git.ts";
 
 type UserData = Record<string, never>;
 type Params = Record<string, never>;
 
+function isValidUnixPath(path: string): boolean {
+  let escape = false;
+  for (let i = 0; i < path.length; i++) {
+    if (!escape && path[i].match(/\s/)) {
+      return false;
+    }
+    if (path[i] === "\\") {
+      escape = !escape;
+    } else {
+      escape = false;
+    }
+  }
+  return true;
+}
+
 export class Source extends BaseSource<Params, UserData> {
+  override getCompletePosition(
+    arg: GetCompletePositionArguments<Params>,
+  ): Promise<number> {
+    for (let i = 0; i < arg.context.input.length; i++) {
+      if (isValidUnixPath(arg.context.input.slice(i))) {
+        return Promise.resolve(i);
+      }
+    }
+    return Promise.resolve(arg.context.input.length);
+  }
   async gatherCandidates(
     args: GatherCandidatesArguments<Params>,
   ): Promise<Candidate<UserData>[]> {
@@ -29,7 +57,7 @@ export class Source extends BaseSource<Params, UserData> {
     }
     return list.split("\n").map((item) => ({
       word: `${root.trim()}/${item.trim()}`,
-      mark: 'file'
+      mark: "file",
     }));
   }
   params(): Params {
