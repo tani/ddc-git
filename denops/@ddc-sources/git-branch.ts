ee428e34075f1b8713df454e4908ad5a6128a98c
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-10-14 21:49:40 +0900

    add getCompletePositionArguments

--- denops/@ddc-sources/git-branch.ts
+++ denops/@ddc-sources/git-branch.ts
@@ -3,28 +3,60 @@
  * This work is licensed under the MIT license. git.io/mit-license
  */
 
-import { BaseSource, Candidate } from "https://lib.deno.dev/x/ddc_vim@v0/types.ts"
-import { GatherCandidatesArguments } from "https://lib.deno.dev/x/ddc_vim@v0/base/source.ts"
-import * as fn from "https://deno.land/x/denops_std@v2.1.1/function/mod.ts"
-import { git } from "../../git.ts"
+import {
+  BaseSource,
+  Candidate,
+} from "https://lib.deno.dev/x/ddc_vim@v0/types.ts";
+import {
+  GatherCandidatesArguments,
+  GetCompletePositionArguments,
+} from "https://lib.deno.dev/x/ddc_vim@v0/base/source.ts";
+import * as fn from "https://deno.land/x/denops_std@v2.1.1/function/mod.ts";
+import { git } from "../../git.ts";
 
-type UserData= Record<string, never>
-type Params = Record<string, never>
+type UserData = Record<string, never>;
+type Params = Record<string, never>;
 
-export class Source extends BaseSource<Params, UserData> {
-  async gatherCandidates(args: GatherCandidatesArguments<Params>) : Promise<Candidate<UserData>[]> {
-    const cwd = await fn.getcwd(args.denops) as string
-    const list = await git(cwd, 'branch')
-    if (!list) {
-      return []
+function isNotWhiteSpace(path: string): boolean {
+  let escape = false;
+  for (let i = 0; i < path.length; i++) {
+    if (!escape && path[i].match(/\s/)) {
+      return false;
     }
-    return list.split("\n").map((item) => ({
-      word: item.replace(/\s*\*\s*/, '').trim(),
-      mark: 'branch'
-    }))
+    if (path[i] === "\\") {
+      escape = !escape;
+    } else {
+      escape = false;
+    }
   }
-  params(): Params {
-    return {}
-  }
+  return true;
 }
 
+export class Source extends BaseSource<Params, UserData> {
+  override getCompletePosition(
+    arg: GetCompletePositionArguments<Params>,
+  ): Promise<number> {
+    for (let i = 0; i < arg.context.input.length; i++) {
+      if (isNotWhiteSpace(arg.context.input.slice(i))) {
+        return Promise.resolve(i);
+      }
+    }
+    return Promise.resolve(arg.context.input.length);
+  }
+  override async gatherCandidates(
+    args: GatherCandidatesArguments<Params>,
+  ): Promise<Candidate<UserData>[]> {
+    const cwd = await fn.getcwd(args.denops) as string;
+    const list = await git(cwd, "branch");
+    if (!list) {
+      return [];
+    }
+    return list.split("\n").map((item) => ({
+      word: item.replace(/\s*\*\s*/, "").trim(),
+      mark: "branch",
+    }));
+  }
+  params(): Params {
+    return {};
+  }
+}
